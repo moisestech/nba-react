3e3f98af9113a7b78f9c78f4a1ebc620e31dcbd7
Moises Sanabria <sesmoi.data@gmail.com>
2020-08-30 08:06:47 -0400

    root js file importing app and getting placed with ReactDOM

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/App'
+
+
+function ColorfulBorder () {
+  return (
+    <ul className='border-container'>
+      <li className='border-item' style={{ background: 'var(--red)'}} />
+      <li className='border-item' style={{ background: 'var(--blue)'}} />
+      <li className='border-item' style={{ background: 'var(--pink)'}} />
+      <li className='border-item' style={{ background: 'var(--yellow)'}} />
+      <li className='border-item' style={{ background: 'var(--aqua)'}} />
+    </ul>
+  )
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ColorfulBorder />
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
