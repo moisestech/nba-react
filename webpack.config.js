92b4b2d33d70df50f325e7df2200ce1bd234069d
Moises Sanabria <sesmoi.data@gmail.com>
2020-08-30 08:00:56 -0400

    entry, output, module rules, mode NODE.ENV, plugins, devServer

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
+    ]
+  },
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: './public/_redirects' }
+      ]
+    })
+  ],
+  devServer: {
+    historyApiFallback: true
+  }
+}
