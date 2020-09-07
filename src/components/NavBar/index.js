0066442368d5d13bdf0d76cd335c8e5c6b0589fa
Moises Sanabria <m@moises.tech>
2020-09-07 08:41:38 -0500

    imp Link and adding Home, Team, Players to="urls"

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
 export default function NavBar(props) {
-  return <div className="container">NavBar</div>;
+  return (
+    <div className="container navbar">
+      <Link to="/">Home</Link>
+      <nav className="nav-links">
+        <Link to="/players">Players</Link>
+        <Link to="/teams">Teams</Link>
+      </nav>
+    </div>
+  );
 }
 
 NavBar.propTypes = {};
