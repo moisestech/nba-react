b34526e4480b0a68e56c57b519246ec002e33719
Moises Sanabria <m@moises.tech>
2020-09-07 04:45:56 -0500

    entry, output, path, filename, publicPath, module, rules, mode, plugins, devServer

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,30 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js',
-    publicPath: '/'
+    path: path.resolve(__dirname, "dist"),
+    filename: "index_bundle.js",
+    publicPath: "/",
   },
   module: {
     rules: [
-      { test: /\.(js)$/, use: 'babel-loader' },
-      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
-    ]
+      { test: /\.(js)$/, use: "babel-loader" },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+    ],
   },
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html'
+      template: "./public/index.html",
     }),
     new CopyPlugin({
-      patterns: [
-        { from: './public/_redirects' }
-      ]
-    })
+      patterns: [{ from: "./public/_redirects" }],
+    }),
   ],
   devServer: {
-    historyApiFallback: true
-  }
-}
+    historyApiFallback: true,
+  },
+};
