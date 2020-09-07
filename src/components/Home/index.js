9c0431cfc65835c85f69b80ef04cf000a587d937
Moises Sanabria <m@moises.tech>
2020-09-07 12:12:51 -0500

    install slug package, fixing home-grid className

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -12,7 +12,7 @@ export default function Home() {
     <div className="container">
       <h1 className="large-header">Has History Basketball League</h1>
       <h3 className="header text-center">Select a team</h3>
-      <div className="home grid">
+      <div className="home-grid">
         {teamNames.map((id) => (
           <Link key={id} to={`/${id}`}>
             <TeamLogo id={id} width="125px" />
