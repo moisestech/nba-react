67c486d5d7d1033148a36618415bd54da8483f8f
Moises Sanabria <m@moises.tech>
2020-09-07 08:40:06 -0500

    imp NavBar, Routes Team, Players

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,8 +1,31 @@
 import * as React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import PropTypes from "prop-types";
+import Home from "../Home";
+import Players from "../Players";
+import Teams from "../Teams";
+import NavBar from "../NavBar";
 
 export default function App() {
-  return <div>Hash History Basketball League</div>;
+  return (
+    <Router>
+      <div>
+        <NavBar />
+
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route path="">
+          <Players />
+        </Route>
+
+        <Route path="/teams">
+          <Teams />
+        </Route>
+      </div>
+    </Router>
+  );
 }
 
 App.propTypes = {};
