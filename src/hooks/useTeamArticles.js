2e6a29c269035879bd09c7675b14fe338e48f57b
Moises Sanabria <m@moises.tech>
2020-09-07 04:53:34 -0500

    useFetch "/articles" "POST" JSON.stringify team

--- src/hooks/useTeamArticles.js
+++ src/hooks/useTeamArticles.js
@@ -1,5 +1,5 @@
-import useFetch from './useFetch'
+import useFetch from "./useFetch";
 
-export default function useTeamsArticles (team) {
-  return useFetch('/articles', 'POST', JSON.stringify({ team }))
+export default function useTeamsArticles(team) {
+  return useFetch("/articles", "POST", JSON.stringify({ team }));
 }
