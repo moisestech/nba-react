d19c4001276df617d1603d707bfbba6fce839e45
Moises Sanabria <m@moises.tech>
2020-09-07 08:42:45 -0500

    updated HtmlWebpackPlugin public/index.html

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import './index.css'
-import App from './components/App'
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
 
-
-function ColorfulBorder () {
+function ColorfulBorder() {
   return (
-    <ul className='border-container'>
-      <li className='border-item' style={{ background: 'var(--red)'}} />
-      <li className='border-item' style={{ background: 'var(--blue)'}} />
-      <li className='border-item' style={{ background: 'var(--pink)'}} />
-      <li className='border-item' style={{ background: 'var(--yellow)'}} />
-      <li className='border-item' style={{ background: 'var(--aqua)'}} />
+    <ul className="border-container">
+      <li className="border-item" style={{ background: "var(--red)" }} />
+      <li className="border-item" style={{ background: "var(--blue)" }} />
+      <li className="border-item" style={{ background: "var(--pink)" }} />
+      <li className="border-item" style={{ background: "var(--yellow)" }} />
+      <li className="border-item" style={{ background: "var(--aqua)" }} />
     </ul>
-  )
+  );
 }
 
 ReactDOM.render(
@@ -21,5 +20,5 @@ ReactDOM.render(
     <ColorfulBorder />
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
