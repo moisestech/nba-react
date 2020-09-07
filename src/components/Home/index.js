a59272d0b9458bda44a60a50975976872eb985ed
Moises Sanabria <m@moises.tech>
2020-09-07 09:09:15 -0500

    imp Link, mapped teamNames id into Links to="id" TeamLogo

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,24 @@
 import React from "react";
-import PropTypes from "prop-types";
+import TeamLogo from "../TeamLogo";
+import { Link } from "react-router-dom";
+import useTeamNames from "../../hooks/useTeamNames";
 
-export default function Home(props) {
-  return <div className="container">Home</div>;
+export default function Home() {
+  const { loading, response: teamNames } = useTeamNames();
+
+  if (loading) return null;
+
+  return (
+    <div className="container">
+      <h1 className="large-header">Has History Basketball League</h1>
+      <h3 className="header text-center">Select a team</h3>
+      <div className="home grid">
+        {teamNames.map((id) => (
+          <Link key={id} to={`/${id}`}>
+            <TeamLogo id={id} width="125px" />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 }
-
-Home.propTypes = {};
