743c5fbee4fe1a882b895ee910930bb494625b1f
Moises Sanabria <m@moises.tech>
2020-09-14 16:31:55 -0500

    teamId useParams, useTeam(teamId) team, TeamLogo, Link team.name

---
+++ src/components/Teams/components/Team/index.js
@@ -0,0 +1,35 @@
+import * as React from "react";
+import TeamLogo from "../../../TeamLogo";
+import { Link, useParams } from "react-router-dom";
+import useTeam from "../../../../hooks/useTeam";
+
+export default function Team() {
+  const { teamId } = useParams();
+
+  const { loading, response: team } = useTeam(teamId);
+
+  if (loading === true) return null;
+
+  return (
+    <div className="panel">
+      <div style={{ width: "100%" }}>
+        <TeamLogo id={team.id} className="center" />
+        <h1 className="medium-header">{team.name}</h1>
+        <ul className="info-list row">
+          <li>
+            Est.<div>{team.established}</div>
+          </li>
+          <li>
+            Manager<div>{team.manager}</div>
+          </li>
+          <li>
+            Coach<div>{team.coach}</div>
+          </li>
+        </ul>
+        <Link className="center btn-main" to={`/${teamId}`}>
+          {team.name} Team Page
+        </Link>
+      </div>
+    </div>
+  );
+}
