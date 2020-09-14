06e245ad56fe973d66e615788b392edc034cf057
Moises Sanabria <m@moises.tech>
2020-09-14 16:24:16 -0500

    content useState, useEffect, setInterval clearInterval, [text]

---
+++ src/components/Loading/index.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+
+export default function Loading({ text = "Loading" }) {
+  const [content, setContent] = useState(text);
+
+  useEffect(() => {
+    const id = window.setInterval(() => {
+      setContent((content) => {
+        return content === `${text}...` ? text : `${content}.`;
+      });
+    }, 500);
+
+    return () => window.clearInterval(id);
+  }, [text]);
+
+  return (
+    <div className="container">
+      <p className="text-center">{content}</p>
+    </div>
+  );
+}
+
+Loading.propTypes = {
+  text: PropTypes.string,
+};
