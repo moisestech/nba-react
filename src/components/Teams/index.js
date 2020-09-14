55f4a5ab747e23887abf6f0ef0766fda49255308
Moises Sanabria <m@moises.tech>
2020-09-14 16:21:30 -0500

    useTeamNames Hook, useRouteMatch path, Route :teamId, sidebar

--- src/components/Teams/index.js
+++ src/components/Teams/index.js
@@ -1,7 +1,31 @@
-import React from "react";
+import * as React from "react";
+import Sidebar from "../Sidebar";
+import Team from "./components/Team";
+import { Route, Switch, useRouteMatch } from "react-router-dom";
+import useTeamNames from "../../hooks/useTeamNames";
+import Loading from "../Loading";
 
-export default function Teams(props) {
-  return <div className="container">Teams</div>;
+export default function Teams() {
+  const { response: teamNames, loading } = useTeamNames();
+  const { path } = useRouteMatch();
+
+  if (loading === true) {
+    return <Loading />;
+  }
+
+  return (
+    <div className="container two-column">
+      <Sidebar title="Teams" list={teamNames} />
+
+      <Switch>
+        <Route path={`${path}/:teamId`}>
+          <Team />
+        </Route>
+
+        <Route path="*">
+          <div className="sidebar-instruction">Select a Team</div>
+        </Route>
+      </Switch>
+    </div>
+  );
 }
-
-Teams.propTypes = {};
